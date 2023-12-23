44209049fdf825f9314e65aa0a9c126ec4c26687
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2023-12-23 21:59:36 +1100

    Created Hero component which included my photo by nextSJ Image and applied styles

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,22 @@
+import Image from "next/image";
+
+import classes from "./hero.module.css";
+
 function Hero() {
-  return <>Hero goes here</>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/manijeh.png"
+          alt="An image showing Manijeh"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I am Manijeh</h1>
+      <p>I am building fullstack applications with NextJS</p>
+    </section>
+  );
 }
 
 export default Hero;
