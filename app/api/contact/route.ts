e56e3c5fd6359ddb0827f018aa650be7ea86ec47
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-01-03 13:13:45 +1100

    Added mongodb

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest } from "next/server"
+import { MongoClient } from "mongodb"
 
 export async function POST(request: NextRequest) {
 	const body = await request.json()
@@ -26,14 +27,43 @@ export async function POST(request: NextRequest) {
 			const newMessage = {
 				email: body.email,
 				name: body.name,
-				message: body.message
+				message: body.message,
+				id: ""
 			}
-			//TODO: Store it in a database
-			console.log(newMessage)
-			return Response.json(
-				{ message: "Successfully stored message!", newMessage },
-				{ status: 201 }
-			)
+			const username = process.env.mongodb_username
+				? encodeURIComponent(process.env.mongodb_username)
+				: ""
+			const password = process.env.mongodb_password
+				? encodeURIComponent(process.env.mongodb_password)
+				: ""
+
+			let client
+			try {
+				client = await MongoClient.connect(
+					`mongodb+srv://${username}:${password}@${process.env.mongodb_clustername}.aurgawe.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`
+				)
+			} catch (error) {
+				return new Response("Couldn't connect to the database!", {
+					status: 500
+				})
+			}
+
+			const db = client.db()
+			try {
+				const result = await db.collection("messages").insertOne(newMessage)
+				newMessage.id = result.insertedId.toString()
+				client.close()
+
+				return Response.json(
+					{ message: "Successfully stored message!", newMessage },
+					{ status: 201 }
+				)
+			} catch (error) {
+				client.close()
+				return new Response("Couldn't store into the database!", {
+					status: 500
+				})
+			}
 		}
 	} else {
 		return new Response("No body request", {
