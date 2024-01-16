b57784bc007f00f9915a558efba49df4c838ca87
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-01-16 22:34:24 +1100

    Replaced the place holder images with screen shots of my projects, updated mock data file accordingly

--- mock-data/index.ts
+++ mock-data/index.ts
@@ -1,13 +1,13 @@
 export const DUMMYـPROJECTS = [
 	{
-		slug: "getting-started-with-nextjs",
-		title: "Getting Started with NextJS",
-		image: "getting-started-nextjs.png",
-		excerpt:
-			"NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+		slug: "game-explorer",
+		title: "Game Lover App",
+		image: "index.png",
+		excerpt: "The Game Lover app in which you can explore many games.",
 		date: "2022-02-10",
 		isFeatured: true,
-		content: "This is a sample content ",
+		content:
+			"Built by NextJS, Filtering by Platform type, and sorting\n			Search box to search among the games\n			Infinite scrolling in the homepage for Games list\n			The Grid component used for the list is optimized for different screens, for responsiveness of the page\n			Click on a Game card, redirects to the Game info page which has games images and video trailer (if exits)\n			If the critics scode is less than 60, the badge is yellow and if higher it's green, indicating the popularity of the game\n			Click on a Game card, redirects to the Game info page (the game info page is not yet implemented)\n			If the rating of a game is less than 3, no emoji, if higher a specific emoji is shown depending on the value of 3, 4 or 5",
 		codeSnippet: {
 			language: "js",
 			code: "const basics = 'Okay, that should not be too difficult actually';\n	function printBasics() {\n		console.log(basics);\n} \nprintBasics();"
@@ -16,7 +16,7 @@ export const DUMMYـPROJECTS = [
 	{
 		slug: "getting-started-with-nextjs2",
 		title: "Getting Started with NextJS 2",
-		image: "getting-started-nextjs.png",
+		image: "index.png",
 		excerpt:
 			"NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
 		date: "2022-02-10",
@@ -26,7 +26,7 @@ export const DUMMYـPROJECTS = [
 	{
 		slug: "getting-started-with-nextjs3",
 		title: "Getting Started with NextJS 3",
-		image: "getting-started-nextjs.png",
+		image: "index.png",
 		excerpt:
 			"NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
 		date: "2022-02-10",
@@ -36,7 +36,7 @@ export const DUMMYـPROJECTS = [
 	{
 		slug: "getting-started-with-nextjs4",
 		title: "Getting Started with NextJS 4",
-		image: "getting-started-nextjs.png",
+		image: "index.png",
 		excerpt:
 			"NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
 		date: "2022-02-10",
