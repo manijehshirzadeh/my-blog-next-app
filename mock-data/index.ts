6590a4b2e773ab21549db20e148bf8f9fae424f5
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-08-19 11:32:14 +1000

    Added post hub project

--- mock-data/index.ts
+++ mock-data/index.ts
@@ -1,38 +1,37 @@
 export const DUMMYـPROJECTS = [
 	{
-		slug: "game-explorer",
-		title: "Game Explorer",
+		slug: "post-hub",
+		title: "Post Hub",
 		image: "index.png",
-		excerpt: "The Game Lover app in which you can explore many games.",
-		date: "2023-12-01",
+		excerpt:
+			"The Post Hub app where you can share your post and see other posts.",
+		date: "2024-06-24",
 		isFeatured: true,
-		demoUrl: "https://game-lover-react-app-by-manijeh.vercel.app/",
-		gitHubUrl:
-			"https://github.com/manijehshirzadeh/my-new-game-lover-react-app",
+		demoUrl: "https://post-hub.onrender.com/",
+		gitHubUrl: "https://github.com/manijehshirzadeh/post-hub",
 		features: [
-			"Search box to search among the games",
-			"Infinite scrolling in the homepage for Games list",
-			"Filtering by Platform type, and sorting",
-			"The Grid component used for the list is optimized for different screens, for responsiveness of the page",
-			"Click on a Game card, redirects to the Game info page which has games images and video trailer (if exits)",
-			"If the critics score is less than 60, the badge is yellow and if higher it's green, indicating the popularity of the game",
-			"Click on a Game card, redirects to the Game info page (the game info page is not yet implemented)",
-			"If the rating of a game is less than 3, no emoji, if higher a specific emoji is shown depending on the value of 3, 4 or 5",
-			"Deployed using vercel"
+			"User authentication and authorization (Sign up, Sign in, Sign out)",
+			"Each user can Create, Update, and Delete their own posts",
+			"Each user can see ALL the posts in the website",
+			"A guest user (user who doesn't sign in) can see ALL the post, as well as can comment on ALL the posts",
+			"Each user who signed in can also comment on ALL the posts",
+			"Deployed using Render"
 		],
-		content:
-			"Built by NextJS, Filtering by Platform type, and sorting\n			Search box to search among the games\n			Infinite scrolling in the homepage for Games list\n			The Grid component used for the list is optimized for different screens, for responsiveness of the page\n			Click on a Game card, redirects to the Game info page which has games images and video trailer (if exits)\n			If the critics scode is less than 60, the badge is yellow and if higher it's green, indicating the popularity of the game\n			Click on a Game card, redirects to the Game info page (the game info page is not yet implemented)\n			If the rating of a game is less than 3, no emoji, if higher a specific emoji is shown depending on the value of 3, 4 or 5",
-
 		technologies: [
-			"NextJs",
-			" React",
-			" JavaScript",
-			" TypeScript",
-			" chakra-UI",
-			" axios",
-			" zustand",
-			" react-icons",
-			"vercel"
+			"Node.js",
+			" Express.js",
+			" express-session",
+			" Cloudinary",
+			" multer",
+			" MongoDB",
+			" Mongoose",
+			" EJS",
+			" Bootstrap",
+			" bcrypt",
+			" dotenv",
+			" method-override",
+			" morgan",
+			" Render"
 		]
 	},
 	{
